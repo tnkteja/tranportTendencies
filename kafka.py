f684701bc57caa82c704cd78d6ce4867af399e2c
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2017-04-28 12:55:41 -0400

    Create kafka.py

---
+++ kafka.py
@@ -0,0 +1,21 @@
+class Producer(object):
+    pass
+    
+class Consumer(object):
+    pass
+    
+class Topic(object):
+   pass
+
+class Partition(object):
+   pass
+
+class Broker(object):
+   pass
+   
+class zookeeper(object);
+   pass
+
+p1=Producer()
+t=Topic()
+c1=Consumer()
